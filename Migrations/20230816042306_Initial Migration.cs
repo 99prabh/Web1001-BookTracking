36b65069c53b366fb3c5f3cdac49adaa4866c204
99prabh <133125732+99prabh@users.noreply.github.com>
2023-08-16 20:23:40 -0400

    Migration Added

---
+++ Migrations/20230816042306_Initial Migration.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web1001_BookTracking.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryTypes",
+                columns: table => new
+                {
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryTypes", x => x.Type);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    NameToken = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoryType = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.NameToken);
+                    table.ForeignKey(
+                        name: "FK_Categories_CategoryTypes_CategoryType",
+                        column: x => x.CategoryType,
+                        principalTable: "CategoryTypes",
+                        principalColumn: "Type",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ISBN = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Category_NameToken = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ISBN);
+                    table.ForeignKey(
+                        name: "FK_Books_Categories_Category_NameToken",
+                        column: x => x.Category_NameToken,
+                        principalTable: "Categories",
+                        principalColumn: "NameToken",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Category_NameToken",
+                table: "Books",
+                column: "Category_NameToken");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryType",
+                table: "Categories",
+                column: "CategoryType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "CategoryTypes");
+        }
+    }
+}
